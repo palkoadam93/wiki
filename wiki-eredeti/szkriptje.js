041899df6aa4f72c247ac46d958095b5b618243b
Palko Adam <palkoadam93@gmail.com>
2019-06-08 10:18:37 +0200

    up

---
+++ wiki-eredeti/szkriptje.js
@@ -0,0 +1,209 @@
+document.documentElement.className = document.documentElement.className.replace( /(^|\s)client-nojs(\s|$)/, "$1client-js$2" );
+		
+(window.RLQ=window.RLQ||[]).push(function() mw.config.set({
+	"wgCanonicalNamespace":"",
+	"wgCanonicalSpecialPageName":false,
+	"wgNamespaceNumber":0,
+	"wgPageName":"Communist_Mutants_from_Space",
+	"wgTitle":"Communist Mutants from Space",
+	"wgCurRevisionId":872394912,
+	"wgRevisionId":872394912,
+	"wgArticleId":1888841,
+	"wgIsArticle":true,
+	"wgIsRedirect":false,
+	"wgAction":"view",
+	"wgUserName":null,
+	"wgUserGroups":["*"],
+	"wgCategories":[
+		"All articles with dead external links",
+		"Articles with dead external links from June 2016",
+		"Articles using Infobox video game using locally defined parameters",
+		"Articles using Wikidata infoboxes with locally defined images",
+		"1982 video games",
+		"Atari 2600 games",
+		"Atari 2600-only games",
+		"Cold War video games",
+		"Fixed shooters",
+		"North America-exclusive video games",
+		"Starpath games",
+		"Video games developed in the United States"],
+	"wgBreakFrames":false,
+	"wgPageContentLanguage":"en",
+	"wgPageContentModel":"wikitext",
+	"wgSeparatorTransformTable":["",""],
+	"wgDigitTransformTable":["",""],
+	"wgDefaultDateFormat":"dmy",
+	"wgMonthNames":["","January","February","March","April","May","June","July","August","September","October","November","December"],
+	"wgMonthNamesShort":["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+	"wgRelevantPageName":"Communist_Mutants_from_Space",
+	"wgRelevantArticleId":1888841,
+	"wgRequestId":"XLPm0QpAMEoAAEsSjooAAAAW",
+	"wgCSPNonce":false,
+	"wgIsProbablyEditable":true,
+	"wgRelevantPageIsProbablyEditable":true,
+	"wgRestrictionEdit":[],
+	"wgRestrictionMove":[],
+	"wgFlaggedRevsParams":{"tags":{}},
+	"wgStableRevisionId":null,
+	"wgBetaFeaturesFeatures":[],
+	"wgMediaViewerOnClick":true,
+	"wgMediaViewerEnabledByDefault":true,
+	"wgPopupsReferencePreviews":false,
+	"wgPopupsShouldSendModuleToUser":true,
+	"wgPopupsConflictsWithNavPopupGadget":false,
+	"wgVisualEditor":{
+		"pageLanguageCode":"en",
+		"pageLanguageDir":"ltr",
+		"pageVariantFallbacks":"en",
+		"usePageImages":true,
+		"usePageDescriptions":true},
+	"wgMFDisplayWikibaseDescriptions":{
+		"search":true,
+		"nearby":true,
+		"watchlist":true,
+		"tagline":false},
+	"wgRelatedArticles":null,
+	"wgRelatedArticlesUseCirrusSearch":true,
+	"wgRelatedArticlesOnlyUseCirrusSearch":false,
+	"wgWMESchemaEditAttemptStepOversample":false,
+	"wgPoweredByHHVM":true,
+	"wgULSCurrentAutonym":"English",
+	"wgNoticeProject":"wikipedia",
+	"wgCentralNoticeCookiesToDelete":[],
+	"wgCentralNoticeCategoriesUsingLegacy":["Fundraising","fundraising"],
+	"wgWikibaseItemId":"Q5154312",
+	"wgScoreNoteLanguages":{
+		"arabic":"العربية",
+		"catalan":"català",
+		"deutsch":"Deutsch",
+		"english":"English",
+		"espanol":"español",
+		"italiano":"italiano",
+		"nederlands":"Nederlands",
+		"norsk":"norsk",
+		"portugues":"português",
+		"suomi":"suomi",
+		"svenska":"svenska",
+		"vlaams":"West-Vlams"},
+	"wgScoreDefaultNoteLanguage":"nederlands",
+	"wgCentralAuthMobileDomain":false,
+	"wgVisualEditorUnsupportedEditParams":["undo","undoafter","veswitched"],
+	"wgEditSubmitButtonLabelPublish":true});
+
+mw.loader.state({
+								 "ext.gadget.charinsert-styles":"ready",
+								 "ext.globalCssJs.user.styles":"ready",
+								 "ext.globalCssJs.site.styles":"ready",
+								 "site.styles":"ready",
+								 "noscript":"ready",
+								 "user.styles":"ready",
+								 "ext.globalCssJs.user":"ready",
+								 "ext.globalCssJs.site":"ready",
+								 "user":"ready",
+								 "user.options":"ready",
+								 "user.tokens":"loading",
+								 "ext.cite.styles":"ready",
+								 "mediawiki.legacy.shared":"ready",
+								 "mediawiki.legacy.commonPrint":"ready",
+								 "mediawiki.toc.styles":"ready",
+								 "wikibase.client.init":"ready",
+								 "ext.visualEditor.desktopArticleTarget.noscript":"ready",
+								 "ext.uls.interlanguage":"ready",
+								 "ext.wikimediaBadges":"ready",
+								 "ext.3d.styles":"ready",
+								 "mediawiki.skinning.interface":"ready",
+								 "skins.vector.styles":"ready"
+								 }
+								);
+
+mw.loader.implement("user.tokens@0tffind",function($,jQuery,require,module){
+	/*@nomin*/
+	mw.user.tokens.set({
+		"editToken":"+\\",
+		"patrolToken":"+\\",
+		"watchToken":"+\\",
+		"csrfToken":"+\\"});
+});
+
+RLPAGEMODULES=[
+	"ext.cite.ux-enhancements",
+	"site",
+	"mediawiki.page.startup",
+	"mediawiki.page.ready",
+	"mediawiki.toc",
+	"mediawiki.searchSuggest",
+	"ext.gadget.teahouse",
+	"ext.gadget.ReferenceTooltips",
+	"ext.gadget.watchlist-notice",
+	"ext.gadget.DRN-wizard",
+	"ext.gadget.charinsert",
+	"ext.gadget.refToolbar",
+	"ext.gadget.extra-toolbar-buttons",
+	"ext.gadget.switcher",
+	"ext.centralauth.centralautologin",
+	"mmv.head",
+	"mmv.bootstrap.autostart",
+	"ext.popups",
+	"ext.visualEditor.desktopArticleTarget.init",
+	"ext.visualEditor.targetLoader",
+	"ext.eventLogging",
+	"ext.wikimediaEvents",
+	"ext.navigationTiming",
+	"ext.uls.eventlogger",
+	"ext.uls.init",
+	"ext.uls.compactlinks",
+	"ext.uls.interface",
+	"ext.quicksurveys.init",
+	"ext.centralNotice.geoIP",
+	"ext.centralNotice.startUp",
+	"skins.vector.js"
+];
+
+mw.loader.load(RLPAGEMODULES);
+
+});
+
+(window.RLQ=window.RLQ||[]).push(
+	function(){
+	mw.config.set({
+		"wgPageParseReport":{
+			"limitreport":{
+				"cputime":"0.252",
+				"walltime":"0.349",
+				"ppvisitednodes":{"value":1089,"limit":1000000},
+				"ppgeneratednodes":{"value":0,"limit":1500000},
+				"postexpandincludesize":{"value":11543,"limit":2097152},
+				"templateargumentsize":{"value":1226,"limit":2097152},
+				"expansiondepth":{"value":12,"limit":40},
+				"expensivefunctioncount":{"value":1,"limit":500},
+				"unstrip-depth":{"value":1,"limit":20},
+				"unstrip-size":{"value":9207,"limit":5000000},
+				"entityaccesscount":{"value":1,"limit":400},
+				"timingprofile":[
+					"100.00%  323.692      1 -total",
+					" 52.65%  170.416      1 Template:Reflist",
+					" 46.42%  150.243      1 Template:Infobox_video_game",
+					" 38.24%  123.780      1 Template:Infobox",
+					" 33.45%  108.266      3 Template:Cite_web",
+					" 14.40%   46.626      1 Template:Dead_link",
+					" 12.02%   38.915      1 Template:Fix",
+					" 10.34%   33.469      2 Template:Category_handler",
+					"  8.08%   26.170     14 Template:If_first_display_both",
+					"  7.50%   24.283      1 Template:Vgrelease"]
+			},
+			"scribunto":{
+				"limitreport-timeusage":{"value":"0.156","limit":"10.000"},
+				"limitreport-memusage":{"value":3109046,"limit":52428800}
+			},
+			"cachereport":{
+				"origin":"mw1247",
+				"timestamp":"20190330202935",
+				"ttl":2592000,
+				"transientcontent":false
+			}
+		}
+	}
+				 );
+	mw.config.set({"wgBackendResponseTime":85,"wgHostname":"mw1239"});
+	}
+);
